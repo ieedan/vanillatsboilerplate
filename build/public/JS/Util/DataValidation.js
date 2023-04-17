e626503068c0cd35edff3c8a94ec7d2c9f5e05a1
Aidan Bleser <117548273+ieedan@users.noreply.github.com>
2023-04-17 13:03:13 -0500

    Changed build script
    -Build script now brings in node_modules as well to avoid issues with depencies

---
+++ build/public/JS/Util/DataValidation.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.IsNullOrEmpty = void 0;
+function IsNullOrEmpty(string) {
+    if (string === null || string === '') {
+        return true;
+    }
+    return false;
+}
+exports.IsNullOrEmpty = IsNullOrEmpty;
+//# sourceMappingURL=DataValidation.js.map
